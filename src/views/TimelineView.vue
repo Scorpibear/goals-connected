78a1c0dcb7e5fca6da11afb8928ac61c1a8ba235
Mikhail Subach <mikhail_subach@epam.com>
2025-01-21 13:41:42 +0300

    refresh timeline when date is changed

--- src/views/TimelineView.vue
+++ src/views/TimelineView.vue
@@ -1,10 +1,18 @@
 <script setup>
+import { ref } from 'vue'
 import GoalsTimeline from '../components/GoalsTimeline.vue'
 import BackendSelector from '@/services/backend/backend-selector'
 
 const backend = BackendSelector.getBackend()
+
+const refreshKey = ref(0)
+
+function onDateChange() {
+  refreshKey.value += 1;
+}
+
 </script>
 
 <template>
-  <GoalsTimeline :backend />
+  <GoalsTimeline :backend :key=refreshKey @date-change="onDateChange" />
 </template>
